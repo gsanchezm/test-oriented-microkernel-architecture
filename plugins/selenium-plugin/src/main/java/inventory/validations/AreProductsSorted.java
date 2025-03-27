8dec22c35bc389a5950d78492ced9b9c6f9fa24a
Gilberto Sanchez <gilbertosanchez@MacBook-Pro-de-Gilberto.local>
2025-03-27 17:54:12 -0600

    Added Validations for inventory

--- plugins/selenium-plugin/src/main/java/inventory/validations/AreProductsSorted.java
+++ plugins/selenium-plugin/src/main/java/inventory/validations/AreProductsSorted.java
@@ -1,4 +1,11 @@
 package inventory.validations;
 
-public class AreProductsSorted {
+import intarfaces.validations.IValidation;
+import utils.BaseLogger;
+
+public class AreProductsSorted extends BaseLogger implements IValidation {
+    @Override
+    public boolean validate(Object... args) {
+        return false;
+    }
 }
